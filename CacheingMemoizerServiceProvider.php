eddbaded4217ef10cdc80b1720a641cd22ccf108
David Jordan <davidj@cyber-duck.co.uk>
2017-01-23 13:01:55 +0000

    Add Service Provider

---
+++ CacheingMemoizerServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cyberduck\LaravelCacheingMemoizer;
+
+use Illuminate\Support\ServiceProvider;
+use CyberDuck\CacheingMemoizer\CacheingMemoizer;
+
+class CacheingMemoizerServiceProvider extends ServiceProvider {
+
+    public function boot()
+    {
+        CacheingMemoizer::addShutdownFunction(function($key, $value){
+            \Cache::put($key, $value, CacheingMemoizer::getCacheDuration());
+        });
+
+        CacheingMemoizer::setCacheRetrievalFunction([\Cache::getFacadeRoot(), 'get']);
+
+        app()->terminating(function(){
+            CacheingMemoizer::shutdown();
+        });
+
+    }
+
+    public function register()
+    {
+        //ooo eee ooo aaa aaa ting tang walla walla wing bang
+    }
+
+}
